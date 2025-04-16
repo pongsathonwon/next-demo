d3deac709f22cb3f3dd84fa48ca875bb1da1c849
pongsathonwon <men1234-2008@hotmail.com>
2025-04-16 12:43:03 +0700

    add styling

--- app/server/page.tsx
+++ app/server/page.tsx
@@ -5,10 +5,13 @@ async function ServerPage() {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = (res.ok ? await res.json() : []) as TPost[];
   return (
-    <div>
+    <div className="flex flex-col gap-4 p-4">
       {data.map(({ id, title, body }) => (
-        <div key={id}>
-          <h2>{title}</h2>
+        <div
+          key={id}
+          className="border border-black rounded-sm p-4 flex flex-col gap-2"
+        >
+          <h2 className="capitalize">{title}</h2>
           <p>{body}</p>
         </div>
       ))}
