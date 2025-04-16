f316c33f40ff368ccbf58be5f7bc83d5aae83412
pongsathonwon <men1234-2008@hotmail.com>
2025-04-16 11:09:27 +0700

    try fetching data from json placeholder in client side

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+// apply use client directive for learning react
+"use client";
+// todo fetch data from https://jsonplaceholder.typicode.com/todos
 export default function Home() {
   return <div></div>;
 }
