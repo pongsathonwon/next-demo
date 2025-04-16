21e7e760d79ea1fc5309e9a21976b61bf1593571
pongsathonwon <men1234-2008@hotmail.com>
2025-04-16 12:43:44 +0700

    add suspense

--- app/server/page.tsx
+++ app/server/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { TPost } from "./post.type";
 // fetch data using server component
 async function ServerPage() {
@@ -6,15 +6,17 @@ async function ServerPage() {
   const data = (res.ok ? await res.json() : []) as TPost[];
   return (
     <div className="flex flex-col gap-4 p-4">
-      {data.map(({ id, title, body }) => (
-        <div
-          key={id}
-          className="border border-black rounded-sm p-4 flex flex-col gap-2"
-        >
-          <h2 className="capitalize">{title}</h2>
-          <p>{body}</p>
-        </div>
-      ))}
+      <Suspense fallback={<div>loading ...</div>}>
+        {data.map(({ id, title, body }) => (
+          <div
+            key={id}
+            className="border border-black rounded-sm p-4 flex flex-col gap-2"
+          >
+            <h2 className="capitalize">{title}</h2>
+            <p>{body}</p>
+          </div>
+        ))}
+      </Suspense>
     </div>
   );
 }
