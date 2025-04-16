daa306f7c01e7be2bc034eb071a62648f5dc392a
pongsathonwon <men1234-2008@hotmail.com>
2025-04-16 11:21:45 +0700

    define fetcher function

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
 // apply use client directive for learning react
 "use client";
+
+import { useEffect } from "react";
+import { TTodo } from "./todo.type";
+
 // todo fetch data from https://jsonplaceholder.typicode.com/todos
 export default function Home() {
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+      if (!res.ok) throw new Error("cannot fetch");
+      const data = (await res.json()) as TTodo[];
+      console.log(data);
+    };
+  }, []);
   return <div></div>;
 }
