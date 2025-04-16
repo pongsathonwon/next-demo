6dfdb41cfa17a52b37d08dbd1819d1a6aa1c0044
pongsathonwon <men1234-2008@hotmail.com>
2025-04-16 11:29:12 +0700

    add clenup fn

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,33 @@
 // apply use client directive for learning react
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { TTodo } from "./todo.type";
 
 // todo fetch data from https://jsonplaceholder.typicode.com/todos
 export default function Home() {
+  const [todo, setTodo] = useState<TTodo[]>([]);
+  const ctrl = new AbortController();
   useEffect(() => {
-    const fetcher = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/todos");
-      if (!res.ok) throw new Error("cannot fetch");
-      const data = (await res.json()) as TTodo[];
-      console.log(data);
+    const fetcher = async (signal: AbortSignal) => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/todos", {
+          signal,
+        });
+        if (!res.ok) throw new Error("cannot fetch");
+        const data = (await res.json()) as TTodo[];
+        setTodo(data);
+      } catch (err) {
+        console.log(err);
+        setTodo([]);
+      }
+    };
+    console.log("use effect");
+    fetcher(ctrl.signal);
+    return () => {
+      console.log("celen up");
+      ctrl.abort();
     };
   }, []);
-  return <div></div>;
+  return <div>{JSON.stringify(todo)}</div>;
 }
